9a1489e27a23bcb5aa5848c24c1204a01b367040
AhmedTrigui1 <ahmed.trigui@oratiotechnologies.com>
2025-07-16 11:47:04 +0200

    fixed main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,22 @@
+# Azure Resource Manager Provider
+provider "azurerm" {
+  features {}
+}
+
+# Kubernetes Provider (configured after AKS cluster creation)
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
 }
 
-provider "aws" {
-  region = var.region
+# Get available Azure regions/locations
+data "azurerm_locations" "available" {
+  location = var.region
 }
 
-data "aws_availability_zones" "available" {}
-
+# Local values for consistency
 locals {
   cluster_name = var.clusterName
 }
-
-##
